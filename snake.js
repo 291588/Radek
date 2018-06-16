8a23c8a1ff2a9175d6d2d4a958e8994e257563d4
291588 <opss4@wp.pl>
2018-06-16 22:45:49 +0200

    pliki audio + znikanie buttonow po przycisnieciu

--- snake.js
+++ snake.js
@@ -2,6 +2,7 @@
 
 
 let wybor;	
+let b = document.body;
 
 let button1 = document.createElement("button");
 let button2 = document.createElement("button");
@@ -17,26 +18,27 @@ document.body.addEventListener("click",function(e){
 
 if(e.target === button1){	
 wybor = 1;	
-button1.style.visibility = "hidden";
-button2.style.visibility = "hidden";
-button3.style.visibility = "hidden";
+b.removeChild(button1);
+b.removeChild(button2);
+b.removeChild(button3);
+
 Gra(1);		
 }	
 	
 else if(e.target === button2){
 wybor = 2;	
-button1.style.visibility = "hidden";
-button2.style.visibility = "hidden";
-button3.style.visibility = "hidden";
+b.removeChild(button1);
+b.removeChild(button2);
+b.removeChild(button3);
 Gra(2);			
 	
 }	
 	
 else if(e.target === button3){
 wybor = 3;
-button1.style.visibility = "hidden";
-button2.style.visibility = "hidden";
-button3.style.visibility = "hidden";
+b.removeChild(button1);
+b.removeChild(button2);
+b.removeChild(button3);
 Gra(3);		
 
 
@@ -45,6 +47,22 @@ Gra(3);
 
 
 function Gra(wybor){
+
+
+
+let koniec = new Audio();
+let jedz = new Audio();
+let gora = new Audio();
+let prawo = new Audio();
+let lewo = new Audio();
+let dol = new Audio();
+
+koniec.src = "koniec.mp3";
+jedz.src = "jedz.mp3";
+gora.src = "gora.mp3";
+prawo.src = "prawo.mp3";
+lewo.src = "lewo.mp3";
+dol.src = "dol.mp3";
 
 
 let i;
@@ -71,14 +89,18 @@ let kierunek;
 
 function sKierunek(e){
 
-if(e.keyCode === 37 && kierunek !== "right"){
-kierunek = "left";}
-if(e.keyCode === 38 && kierunek !== "down"){
-kierunek = "up";}
-if(e.keyCode === 39 && kierunek !== "left"){
-kierunek = "right";}
-if(e.keyCode === 40 && kierunek !== "up"){
-kierunek = "down";}
+if(e.keyCode === 37 && kierunek !== "prawo"){
+kierunek = "lewo";
+lewo.play();}
+if(e.keyCode === 38 && kierunek !== "dol"){
+kierunek = "gora";
+gora.play();}
+if(e.keyCode === 39 && kierunek !== "lewo"){
+kierunek = "prawo";
+prawo.play();}
+if(e.keyCode === 40 && kierunek !== "gora"){
+kierunek = "dol";
+dol.play();}
 
 }
 
@@ -190,12 +212,14 @@ Torus();
 
 if(wybor === 1 ){
 if(snakeX < 0 || snakeY < 0 || snakeX >= cvsW/snakeW || snakeY >= cvsH/snakeH){
+koniec.play();
 alert("GAME OVER!");
 location.reload();
 }
 }
 if(wybor === 2 ){
 if(snakeX < 0 || snakeY < 0 || snakeX >= cvsW/snakeW || snakeY >= cvsH/snakeH){
+koniec.play();
 alert("GAME OVER!");
 location.reload();
 }
@@ -204,25 +228,27 @@ location.reload();
 
 if(wybor === 1 ){
 if(Kolizja(snakeX,snakeY,snake)){
+koniec.play();
 alert("GAME OVER!");
 location.reload();
 }
 }
 if(wybor === 3 ){
 if(Kolizja(snakeX,snakeY,snake)){
+koniec.play();
 alert("GAME OVER!");
 location.reload();
 }
 }
 
-if(wybor === 2){
+if(wybor === 2){	
 jedzOgon(snakeX,snakeY,snake);
 }
 
-if( kierunek === "left"){snakeX -= 1;}
-else if( kierunek === "up"){snakeY -= 1;}
-else if( kierunek === "right"){snakeX += 1;}
-else if( kierunek === "down"){snakeY += 1;}
+if( kierunek === "lewo"){snakeX -= 1;}
+else if( kierunek === "gora"){snakeY -= 1;}
+else if( kierunek === "prawo"){snakeX += 1;}
+else if( kierunek === "dol"){snakeY += 1;}
 
 let nowaGlowa = {
 x: snakeX,
@@ -231,6 +257,7 @@ y: snakeY
 
 
 if(snakeX === food.x && snakeY === food.y){
+jedz.play();
 food = {
 x: Math.round(Math.random()*((cvsW/snakeW-1)+1)),
 y: Math.round(Math.random()*((cvsH/snakeH-1)+1))
